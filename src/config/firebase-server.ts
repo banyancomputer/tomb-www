1a88da7bbef099d30ed3744089300f0ed1095042
amiller68 <alexanderscottmiller@gmail.com>
2023-07-06 15:47:18 -0400

    fix: docker host used for firestore

--- src/config/firebase-server.ts
+++ src/config/firebase-server.ts
@@ -9,18 +9,13 @@ let adminConfig = {
 	} as ServiceAccount),
 };
 
-if (process.env.NODE_ENV === 'development') {
-	console.log('Using local emulator for Firestore: localhost:8080');
-	process.env['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080';
-}
-
 if (!admin.apps.length) {
 	console.log('Initializing Firebase Admin...');
 	console.log('FIREBASE_PROJECT_ID: ', process.env.FIREBASE_PROJECT_ID);
 	console.log('FIREBASE_CLIENT_EMAIL: ', process.env.FIREBASE_CLIENT_EMAIL);
 	console.log(
 		'Using local emulator for Firestore: ',
-		process.env.FIRESTORE_EMULATOR_HOST || 'N/A'
+		process.env.FIRESTORE_EMULATOR_HOST || 'localhost:8080'
 	);
 
 	admin.initializeApp(adminConfig);
